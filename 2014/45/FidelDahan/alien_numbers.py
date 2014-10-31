5b93bfd578c5b0eef651ebf892e336183ec234ea
Rusi Filipov <rfilipov@gmail.com>
2014-10-31 12:31:35 +0100

    Translate basic case for equivalent languages

--- 2014/45/FidelDahan/alien_numbers.py
+++ 2014/45/FidelDahan/alien_numbers.py
@@ -7,22 +7,47 @@ class AlienLanguage(object):
     super().__init__()
     self.alphabet = alphabet
 
+  def create_number(self, value):
+    return AlienNumber(self, value)
+
+  def index_of(self, digit):
+    return self.alphabet.index(digit)
+
+  def digit_at(self, index):
+    return self.alphabet[index]
+
+  def __eq__(self, other):
+    return self.alphabet == other.alphabet
+
 
 class AlienNumber(object):
-  def __init__(self, value:str, language:AlienLanguage):
-    self.value = value
+  def __init__(self, language:AlienLanguage, value:str):
     self.language = language
+    self.value = value
+
+  def map_to(self, target_language):
+    def map_digit(digit):
+      index = self.language.index_of(digit)
+      return target_language.digit_at(index)
+
+    target_value = "".join([map_digit(digit) for digit in self.value])
+    return AlienNumber(target_language, target_value)
+
+  def __eq__(self, other):
+    if self.language == other.language:
+      if self.value == other.value:
+        return True
+    return False
 
 
 class Translation(object):
-  def __init__(self,
-               number:AlienNumber,
-               source:AlienLanguage,
-               target:AlienLanguage):
+  def __init__(self, source_number:AlienNumber, target_language:AlienLanguage):
     super().__init__()
-    self.number = number
-    self.source = source
-    self.target = target
+    self.source_number = source_number
+    self.target_language = target_language
+
+  def translate(self):
+    return self.source_number.map_to(self.target_language)
 
 
 def parse_input(lines):
@@ -34,14 +59,18 @@ def parse_input(lines):
     tokens = line.split()
     target = AlienLanguage(tokens[2])
     source = AlienLanguage(tokens[1])
-    number = AlienNumber(tokens[0], source)
-    return Translation(number, source, target)
+    number = source.create_number(tokens[0])
+    return Translation(number, target)
 
   return [parse_line(line) for line in lines]
 
 
 def solve_problem():
+  # FIXME: complete implementation
   translations = parse_input(fileinput.input())
+  for translation in translations:
+    target_number = translation.translate()
+    print(target_number.value)
 
 
 if __name__ == "__main__":
--- 2014/45/FidelDahan/alien_numbers_test.py
+++ 2014/45/FidelDahan/alien_numbers_test.py
@@ -5,19 +5,63 @@ from alien_numbers import *
 
 
 class InputParsing(unittest.TestCase):
+  """
+  Tests for the parsing of the input text into objects.
+  """
+
   def test_input_parsing(self):
     translations = parse_input(["2",
                                 "a ab cd",
                                 "w wx yz"])
     self.assertEqual(2, len(translations))
     t0 = translations[0]
-    self.assertEqual("a", t0.number.value)
-    self.assertEqual("ab", t0.source.alphabet)
-    self.assertEqual("cd", t0.target.alphabet)
+    self.assertEqual("a", t0.source_number.value)
+    self.assertEqual("ab", t0.source_number.language.alphabet)
+    self.assertEqual("cd", t0.target_language.alphabet)
     t1 = translations[1]
-    self.assertEqual("w", t1.number.value)
-    self.assertEqual("wx", t1.source.alphabet)
-    self.assertEqual("yz", t1.target.alphabet)
+    self.assertEqual("w", t1.source_number.value)
+    self.assertEqual("wx", t1.source_number.language.alphabet)
+    self.assertEqual("yz", t1.target_language.alphabet)
+
+
+class ObjectEquality(unittest.TestCase):
+  def test_alien_language_equality(self):
+    self.assertEqual(AlienLanguage("a"), AlienLanguage("a"))
+    self.assertEqual(AlienLanguage("ab"), AlienLanguage("ab"))
+    self.assertNotEqual(AlienLanguage("X"), AlienLanguage("x"))
+
+  def test_alien_number_equality(self):
+    self.assertEqual(AlienNumber(AlienLanguage("ab"), "a"),
+                     AlienNumber(AlienLanguage("ab"), "a"))
+    self.assertNotEqual(AlienNumber(AlienLanguage("ab"), "a"),
+                        AlienNumber(AlienLanguage("ab"), "b"))
+    self.assertNotEqual(AlienNumber(AlienLanguage("ab"), "a"),
+                        AlienNumber(AlienLanguage("ax"), "a"))
+
+
+class TranslationAlgorithm(unittest.TestCase):
+  """
+  Tests for the translation algorithm, which maps a number
+  from one language in another number in another language.
+  """
+
+  def test_identity_for_same_languages(self):
+    def translate(value:str):
+      return Translation(AlienLanguage("ab").create_number(value),
+                         AlienLanguage("ab")).translate()
+
+    self.assertEqual("a", translate("a").value)
+    self.assertEqual("b", translate("b").value)
+    self.assertEqual("ab", translate("ab").value)
+
+  def test_mapping_for_equivalent_languages(self):
+    def translate(value:str):
+      return Translation(AlienLanguage("ab").create_number(value),
+                         AlienLanguage("AB")).translate()
+
+    self.assertEqual("A", translate("a").value)
+    self.assertEqual("B", translate("b").value)
+    self.assertEqual("AB", translate("ab").value)
 
 
 if __name__ == '__main__':
